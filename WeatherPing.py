5fa3c3e0fe01429ecec4024a8bf574c61458cf6c
Chris <christwellman@gmail.com>
2023-09-14 12:42:25 -0400

    Initial commit with weather script and GitHub Actions setup

---
+++ WeatherPing.py
@@ -0,0 +1,30 @@
+import requests
+
+# Replace "YOUR_API_KEY" with your actual API key
+API_KEY = ""
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+def get_weather_by_zip(zip_code, country_code="us"):
+    response = requests.get(BASE_URL, params={
+        "zip": f"{zip_code},{country_code}",
+        "appid": API_KEY
+    })
+    return response.json()
+
+def get_weather_by_address(address):
+    response = requests.get(BASE_URL, params={
+        "q": address,
+        "appid": API_KEY
+    })
+    return response.json()
+
+if __name__ == "__main__":
+    # Get weather by ZIP code
+    zip_code = input("Enter ZIP code: ")
+    weather_data = get_weather_by_zip(zip_code)
+    print(weather_data)
+
+    # Get weather by address
+    address = input("Enter address: ")
+    weather_data = get_weather_by_address(address)
+    print(weather_data)
